82af98a7ecd2e2a1c60fd9d5602fc718738dd60b
Jaivik Jariwala <112539487+Jaivik-Jariwala@users.noreply.github.com>
2024-05-01 00:04:53 +0530

    Create app.py

---
+++ flask/app.py
@@ -0,0 +1,16 @@
+# app.py
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello, World! This is my Flask app.'
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
